14e306151c8c61f680c24ef6c24f426bd09d4555
oussamaza25 <oussama.zahdi25@gmail.com>
2025-01-31 17:39:29 +0100

    version test

---
+++ Mandatory/main.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ozahdi <ozahdi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 20:39:17 by sslaoui           #+#    #+#             */
+/*   Updated: 2025/01/31 17:13:18 by ozahdi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	l()
+{
+	system("leaks cub3d");
+}
+
+void	free_map(char **map)
+{
+	int	i;
+
+	i = 0;
+	while (map[i])
+	{
+		free(map[i]);
+		i++;
+	}
+	free(map);
+	map = NULL;
+}
+
+int main(int ac, char **av)
+{
+	t_data	utils;
+	t_player	pl;
+	t_graph	mlx;
+	int	fd;
+
+	if (ac != 2)
+		return printf("bad arg!"), 1;
+	utils.player = &pl;
+	utils.mlx = &mlx;
+	utils_init(&utils, av[1]);
+
+	if(ft_strncmp(parsing_map(&utils, &fd),"error",5) == 0)
+		ft_exit(&utils,"Error:\nThe map is Invalid\n",1);
+	if (!utils.map)
+		return (0);
+	player_detection(utils.map, &pl);
+	if ((int)pl.pl_x == 0 || (int)pl.pl_y == 0)
+    {
+		write(2, "Error:\nYou should to add player!\n", 41);
+		return (free_map(utils.map), 1);
+    }
+	ray_casting(&utils, &mlx);
+	free_map(utils.map);
+}
---
+++ Mandatory/parsing/checker.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sslaoui <sslaoui@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 14:59:50 by sslaoui           #+#    #+#             */
+/*   Updated: 2025/01/29 21:30:00 by sslaoui          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub3d.h"
+
+int	parse1(char *str, t_data *utils)
+{
+	if ((*str == '0' || *str == '1')
+		&& (!utils->NO || !utils->EA || !utils->WE || !utils->SO))
+		return (1);
+	if ((*str == '0' || *str == '1')
+		&& (utils->C_rgb == -1 || utils->F_rgb == -1))
+		return (1);
+	return (0);
+}
+
+int	checker(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != '0' && str[i] != '1'
+			&& str[i] != 'N' && str[i] != 'S' && str[i] != 'W'
+			&& str[i] != 'E' && str[i] != ' ' && str[i] != '\n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	parse_dir(char *str, int *i, int in, t_data *utils)
+{
+	int	j;
+
+	in = direction_fill(str, utils, i);
+	j = dir_fill(str, utils, i);
+	if (in)
+	{
+		if (in == 2)
+			return (2);
+		else if (in == 1)
+			return (1);
+	}
+	else if (j)
+	{
+		if (j == 2)
+			return (2);
+		else if (j == 1)
+			return (1);
+	}
+	return (0);
+}
+
+int	parse(char *str, int *i, t_data *utils, int in)
+{
+	int	res;
+
+	res = parse_dir(str, i, in, utils);
+	if (res)
+	{
+		if (res == 2)
+			;
+		else if (res == 1)
+			return (1);
+	}
+	else if ((*str == '0' || *str == '1')
+		&& (!utils->NO || !utils->EA || !utils->WE || !utils->SO))
+		return (1);
+	else if ((*str == '0' || *str == '1')
+		&& (utils->C_rgb == -1 || utils->F_rgb == -1))
+		return (1);
+	else if (space_skip(str) == 1 || ft_strcmp(str, "\n") == 0)
+		return (0);
+	else if (checker(str) == 1)
+		return (0);
+	else
+		return (1);
+	return (0);
+}
+
+int	get_content(int *fd, t_data *utils)
+{
+	char	*str;
+	int		j;
+	int		in;
+
+	j = 0;
+	in = 0;
+	*fd = open(utils->name, O_RDONLY, 0777);
+	if (*fd == -1)
+		return (1);
+	str = get_next_line(*fd);
+	if (line_check(str, utils, in, *fd) == 1)
+		return (1);
+	return (0);
+}
